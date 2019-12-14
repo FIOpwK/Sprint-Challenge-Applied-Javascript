8226a36552f8c744538421d3f437a9a29aed49b0
fiopwk <flopwiki@gmail.com>
2019-12-13 21:01:55 -0500

    Card/index.js: setup cardCreation function

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -4,16 +4,50 @@
 // Stduy the response data you get back, closely.
 // You will be creating a component for each 'article' in the list.
 // This won't be as easy as just iterating over an array though.
-// Create a function that will programmatically create the following DOM component:
 //
-// <div class="card">
-//   <div class="headline">{Headline of article}</div>
-//   <div class="author">
-//     <div class="img-container">
-//       <img src={url of authors image} />
-//     </div>
-//     <span>By {authors name}</span>
 //   </div>
 // </div>
 //
+
+
 // Create a card for each of the articles and add the card to the DOM.
+const cards_article = document.querySelector('.cards-container')
+
+// Create a function that will create DOM components
+// comp ~ component(s)
+function cardCreation(comp) {
+    
+    // <div class="card">
+    const card = document.createElement('div')
+    card.classList.add('card')
+    
+    //   <div class="headline">{Headline of article}</div>
+    const headline = document.createElement('div')
+    
+    //   <div class="author">
+    const author = document.createElement('div')
+    author.classList.add('author')
+    
+    //     <div class="img-container">
+    const img_container = document.createElement('div')
+    img_container.classList.add('img-container')
+    
+    //       <img src={url of authors image} />
+    const image_of_authors = document.createElement('img')
+    image_of_authors.src = '';
+    //     </div>
+    //     <span>By {authors name}</span>
+    const span = document.createElement('span')
+    span.textContent = `By ${res.name}`; 
+
+
+    card.appendChild(headline)
+    card.appendChild(author)
+    author.appendChild(img_container)
+    author.appendChild(span)
+    img_container.appendChild(image_of_authors)
+    
+    
+    return card;
+}
+
